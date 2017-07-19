f2a131d7c1b25c871ab4c47f8d0afe7b3414471c
yuralubinec <yuralubinec@gmail.com>
2017-07-19 17:26:39 +0300

    added success message, and show password checkbox

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -11,6 +11,8 @@ import { User } from '../models/user';
 })
 export class RegistrationComponent implements OnInit {
 
+  private operationIsSuccess: boolean;
+  private display: string = 'password';
   private registrationForm: FormGroup;
   private roles: Array<any>;
   private serverValidationError: string;
@@ -19,7 +21,7 @@ export class RegistrationComponent implements OnInit {
   constructor(private fb: FormBuilder, private registrationService: RegistrationService) { }
 
   ngOnInit() {
-    
+
     this.registrationService.getAvailableRoles().subscribe(response => this.roles = response.json());
     this.createEmptyForm();
 
@@ -30,27 +32,36 @@ export class RegistrationComponent implements OnInit {
     this.registrationForm = this.fb.group({
       username: [null, [Validators.required, Validators.pattern('[a-zA-z]*'), Validators.minLength(4), Validators.maxLength(15)]],
       password: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(15)]],
-      role: [null , [Validators.required]],
-      fullName: [null, [Validators.required, Validators.pattern('[а-яА-Я]|[єюїЄЮЇ]+'),Validators.maxLength(25)]],
+      role: [null, [Validators.required]],
+      fullName: [null, [Validators.required, Validators.pattern('([а-яА-Я]|[єюїіЄЮЇІ])+\\s([а-яА-Я]|[єюїіЄЮЇІ])+'), Validators.maxLength(25)]],
     })
   }
 
   onSubmit() {
+    this.serverValidationError = null;
     this.registrationService.registerNewUser(
       new User(this.registrationForm.value.username, this.registrationForm.value.password,
         this.registrationForm.value.role, this.registrationForm.value.fullName))
-      .then(() => this.createEmptyForm()).catch((error)=>this.handleError(error))
+      .then(() => { this.createEmptyForm(); this.operationIsSuccess = true; setTimeout(() => this.operationIsSuccess = false, 5000) }).catch((error) => this.handleError(error))
   }
 
   handleError(error: any) {
-    if(error.status == 400){
-      console.log(error.body);
+    if (error.status == 400) {
       this.serverValidationError = error._body;
     }
     else {
-      this.serverValidationError == 'Сталася невідома помилка'+ error.status;
+      this.serverValidationError == 'Сталася невідома помилка' + error.status;
     }
-   
+
+  }
+
+  displayPassword(checked: boolean) {
+    if (checked) {
+      this.display = 'text';
+    }
+    else {
+      this.display = 'password';
+    }
   }
 
 }
